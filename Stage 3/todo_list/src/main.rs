926fd2aa32e1d8ea64b948e4573131e928036997
tad1 <tadziobrzeski@gmail.com>
2023-02-22 22:23:10 +0100

    Add serialization

--- Stage 3/todo_list/src/main.rs
+++ Stage 3/todo_list/src/main.rs
@@ -1,5 +1,6 @@
 
-use std::{io::{self, Write}, process::Command};
+use std::{io::{self, Write, Read}, process::Command, path::Path, fs::File};
+use serde::{Serialize, Deserialize};
 
 // Guide
 // - Start with a simple idea, don't think about making that modular (you will clean your code in next iteration)
@@ -9,6 +10,9 @@ use std::{io::{self, Write}, process::Command};
 
 // Modifications:
 // - Save the list, in cache, or executed directory
+///         for save instead of making it manual, try find existing solution.
+///         If you are not famillar with concept of serialization check:
+///             find a good resource about overall conecpts in serialization
 //      - and add it to your DevTools folder
 // - Allow your to navigate thought the list
 // Create separate buffer (add link)
@@ -19,6 +23,26 @@ use std::{io::{self, Write}, process::Command};
 // - For futher modifications: "Why you would use app, insead of text file?"
 
 
+#[derive(Serialize, Deserialize)]
+struct ToDoList {
+    list: Vec<Item>
+}
+
+impl ToDoList {
+    fn load(path : &Path){
+        
+    }
+    fn save(path : &Path){
+        
+    }
+    fn new() -> ToDoList{
+        ToDoList { list: vec![] }
+    }
+}
+
+//TODO: add deserializer
+
+#[derive(Serialize, Deserialize)]
 struct Item {
     title : String,
     completed : bool
@@ -42,13 +66,13 @@ fn main() {
         }
     }
 
-    let mut list : Vec<Item> = vec![];
+    let mut list : ToDoList = ToDoList::new();
     let mut input = String::new();
 
     loop {
         clear();
         println!("Your List: ");
-        for element in &list {
+        for element in &list.list {
             println!("{}", element);
         }
         print!("\n> ");
@@ -58,7 +82,7 @@ fn main() {
         
         let values = input.split_once(" ");
         if let Some(("add", commmand)) = values {
-            list.push(Item { title: commmand.trim_end().to_string(), completed: false });
+            list.list.push(Item { title: commmand.trim_end().to_string(), completed: false });
         } else if let Some(("toggle", index)) = values{
             let val = index.trim().parse::<usize>();
 
@@ -66,13 +90,23 @@ fn main() {
                 println!("{} is not an integer", index);   
             } else {
                 let val = val.unwrap();
-                if val < list.len() {
-                    list[val].completed = !list[val].completed;
+                if val < list.list.len() {
+                    list.list[val].completed = !list.list[val].completed;
                 } else {
                     println!("Out of index");
                 }
             }
-
+        } else if let Some(("save", _)) = values {
+            let save = serde_json::to_string(&list).expect("Can't serialize data!");
+            let mut buffer = File::create("./todo.json").expect("Can't create file!");
+            buffer.write(save.as_bytes()).expect("Can't wire to buffer");
+        } else if let Some(("load", _)) = values {
+            let res = File::open("./todo.json");
+            if let Ok(mut file) = res {
+                let mut buffer = String::new();
+                file.read_to_string(&mut buffer).expect("Can't read from file");
+                list = serde_json::from_str(&buffer).unwrap();
+            }
         }
         
 
