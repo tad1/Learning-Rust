ea5ebe933bfa76aee6cf351fdac5781af2fab0b7
tad1 <tadziobrzeski@gmail.com>
2023-03-14 20:58:10 +0100

    Finish API and TUI

--- Stage 3/todo_list/src/lib.rs
+++ Stage 3/todo_list/src/lib.rs
@@ -1,11 +1,17 @@
 
 use serde::{Serialize, Deserialize};
-use std::{io::{self, Write, Read}, process::Command, path::Path, fs::File, error::Error};
+use std::{io::{self, Write, Read}, process::Command, path::Path, fs::File, error::Error, slice::SliceIndex};
 
 
 macro_rules! assert_todo_in_range {
     ($list:ident, $index:ident) => {
         if($index > $list.len() || $list.len() == 0) {
+            println!("Out of range!");
+            return;
+        }
+    };
+    ($list:ident, $index:expr) => {
+        if(($index) as usize > $list.len() || $list.len() == 0) {
             println!("Out of range!");
             return;
         }
@@ -39,14 +45,13 @@ impl std::fmt::Display for Item {
 
 impl ToDoList {
 
-
     pub fn clear(&mut self){
         self.items.clear();
     }
     
     pub fn load(&mut self, path : &Path) -> Result<(), io::Error>{
         self.clear();
-        let mut file = File::open(path.join("todo.json"))?;
+        let mut file = File::open(path)?;
         let mut buffer = String::new();
         file.read_to_string(&mut buffer).expect("Can't read from file");
         let list : ToDoList = serde_json::from_str(&buffer).expect("Can't deserialize file");
@@ -55,7 +60,7 @@ impl ToDoList {
     }
     pub fn save(&self, path : &Path){
         let save = serde_json::to_string(&self).expect("Can't serialize data!");
-        let mut buffer = File::create(path.join("todo.json")).expect("Can't create file!");
+        let mut buffer = File::create(path).expect("Can't create file!");
         buffer.write(save.as_bytes()).expect("Can't write to buffer");
     }
 
@@ -72,6 +77,10 @@ impl ToDoList {
         }
     }
 
+    pub fn push(&mut self, element : Item) {
+        self.items.push(element);
+    }
+
     pub fn len(&self) -> usize {
         self.items.len()
     }
@@ -86,8 +95,23 @@ impl ToDoList {
         self.items[index].completed = !self.items[index].completed;
     }
 
+    pub fn set(&mut self, index : usize, value : bool) {
+        assert_todo_in_range!(self, index);
+        self.items[index].completed = value;
+    }
+
     pub fn edit(&mut self, index : usize, value : &String) {
-        
+        assert_todo_in_range!(self, index);
+        self.items[index].title = value.to_string();
+    }
+
+    pub fn r#move(&mut self, index : usize, offset : i8) {
+        assert_todo_in_range!(self, index);
+        assert_todo_in_range!(self, index as i16 - offset as i16);
+
+        let item = self.items.remove(index);
+        let insert_index = index as i16 - offset as i16;
+        self.items.insert(insert_index as usize, item);
     }
 
     pub fn new() -> ToDoList{
--- Stage 3/todo_list/src/main.rs
+++ Stage 3/todo_list/src/main.rs
@@ -1,13 +1,14 @@
 
-use std::{io::{self, Write, Read}, process::Command, path::Path, fs::File, error::Error};
+use std::{io::{self, Write, Read}, process::Command, path::{Path, PathBuf}, fs::File, error::Error};
+use regex::Regex;
 use serde::{Serialize, Deserialize};
 use tui::{backend::CrosstermBackend, Terminal};
 use crossterm::{
-    event::{read, Event, KeyCode, KeyEventKind, KeyModifiers},
-    execute, 
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode}
+	event::{read, Event, KeyCode, KeyEventKind, KeyModifiers},
+	execute, 
+	terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode}
 };
-use clap::Parser;
+use clap::{Parser, Subcommand, ValueEnum};
 
 // Guide
 // - Start with a simple idea, don't think about making that modular (you will clean your code in next iteration)
@@ -50,143 +51,280 @@ use tdl::{Item, ToDoList};
 
 #[derive(Debug, Parser)]
 struct Cli {
-    #[arg(default_value = "visual")]
-    command : String
+	/// Set a custom todo file.
+	#[arg(short, long, value_name = "FILE")]
+	file: Option<PathBuf>,
+	#[command(subcommand)]
+	command: Option<Commands>
 }
 
+#[derive(Subcommand, Debug, PartialEq)]
 enum Commands{
-    Exit,
-    Add(String),
-    Delete(usize),
-    Toggle(usize),
-    Navigate
+	Visual,
+	/// Add item with <NAME> to the list 
+	Add {
+		/// items needed to be added, to the list, separated with space
+		names :Vec<String>
+	},
+	Toggle {
+		mode : Option<ToggleMode>,
+		#[command(subcommand)]
+		reference: ReferenceMode,
+
+	},
+	///Print out list to stdout buffer
+	Print,
+	Remove {
+		#[command(subcommand)]
+		reference: ReferenceMode,
+
+	},
+	Edit {
+		index : u16,
+		value : String
+	},
+	Move {
+		index : u8,
+		offset : i8
+	},
+
+	// * What about adding groups?
+	// * That could be a nice addition to this project
 }
 
+#[derive(Debug, PartialEq, Subcommand, Clone)]
+enum ReferenceMode{
+	Indexes {
+		indexes: Vec<usize>
+	},
+	Pattern {
+		pattern: String
+	}
+}
+
+#[derive(Clone, Debug, ValueEnum, PartialEq)]
+enum ToggleMode {
+	On,
+	Off,
+	Swap
+}
 macro_rules! print_list {
-    ($list:ident, $index:ident) => {
-        clear();
-        println!("Your List: ");
-        let mut i = 0;
-        for element in &($list.items) {
-            if i == $index { print!(">")} else {print!(" ")}
-            println!("{}", element);
-            i += 1;
-        }
-    };
+	($list:ident, $index:ident) => {
+		clear();
+		println!("Your List: ");
+		let mut i = 0;
+		for element in &($list.items) {
+			if i == $index { print!(">")} else {print!(" ")}
+			println!("{}", element);
+			i += 1;
+		}
+		println!("\nx - delete, a - append, p - prepend, SPACE - toggle");
+		println!("\tEnter: accept text in item");
+		println!("Navigation: up/down or j/k, CTRL to move items");
+		println!("ESC to exit");
+	};
+}
+
+//*NOTE: I'm using regex here!
+fn handle_command(list : &mut ToDoList,command : Commands) {
+	match command {
+    Commands::Visual => {},
+    Commands::Add { names } => {
+		for name in names{
+			list.push(Item { title: name, completed: false });
+		}
+	},
+    Commands::Toggle { mode, reference } => {
+		let _mode = mode.unwrap_or(ToggleMode::Swap);
+		let value = if _mode == ToggleMode::On {true} else {false};
+		
+		match reference {
+    		ReferenceMode::Indexes { indexes } => {
+				for index in indexes {
+					//check if regex passes
+					if _mode == ToggleMode::Swap {
+						list.toggle(index as usize);
+					} else {
+						list.set(index as usize, value);
+					}
+				}
+			},
+    		ReferenceMode::Pattern { pattern } => {
+				let re = Regex::new(pattern.as_str()).unwrap();
+				for mut item in list.items.iter_mut() {
+					if re.is_match(&item.title) {
+						// toggle
+						if _mode == ToggleMode::Swap {
+							item.completed = !item.completed;
+						} else {
+							item.completed = value;
+						}
+					}
+				}
+				
+			},
+		}
+	},
+    Commands::Print => {
+		for element in &list.items {
+			println!("{}", element);
+		}
+	},
+    Commands::Remove { reference } => {
+		//Check if pattern is set
+		match reference {
+    		ReferenceMode::Indexes { indexes } => {
+				for index in indexes {
+					list.delete(index);
+				}
+			},
+    		ReferenceMode::Pattern { pattern } => {
+				let re = Regex::new(pattern.as_str()).unwrap();
+				list.items.retain(|item| !re.is_match(&item.title));
+			},
+}
+	},
+    Commands::Edit { index, value } => {
+		list.edit(index as usize, &value);
+	},
+    Commands::Move { index, offset } => {
+		list.r#move(index as usize, offset);
+	},
+}
 }
 
 // UI, handle adding new element in nice way
+fn handle_cli(args : Cli) -> Result<(), io::Error> {
+	let path = args.file.unwrap_or(PathBuf::from("./todo.json"));
+	let command = args.command.expect("No command given");
+	let mut list : ToDoList = ToDoList::new();
+	list.load(&path)?;
+
+	handle_command(&mut list, command);
+	list.save(&path);
+	Ok(())
+}
+
+fn clear(){
+	if cfg!(target_os = "windows") {
+		Command::new("cmd").arg("/C").arg("cls").status().expect("Can't execute 'cls' command");
+	} else {
+		Command::new("sh").arg("-C").arg("clear").status().expect("Can't execute 'clear' command");
+	}
+}
+
+fn handle_visual(args : Cli) -> Result<(), io::Error> {
+
+	let mut list : ToDoList = ToDoList::new();
+	let mut input = String::new();
+	let mut index : usize = 0; //Why not to save that also?
+	let path_buf : PathBuf = args.file.unwrap_or(PathBuf::from("./todo.json"));
+	let path: &Path = path_buf.as_path();
+	execute!(io::stdout(), EnterAlternateScreen)?;
+	list.load(&path)?;
+	print_list!(list, index);
+
+	loop {
+		print!("\n> ");
+		let _ = io::stdout().flush();
+		input.clear();
+		// Need introduce modes
+		match read()? {
+			Event::FocusGained => {
+				
+			},
+			Event::FocusLost => {
+				
+			},
+			Event::Key(event) => {
+				if event.kind == KeyEventKind::Press  {
+					match event.code {
+						KeyCode::Up | KeyCode::Char('k') => {
+							//If with CTRL, move element
+							if event.modifiers == KeyModifiers::CONTROL && index > 0{
+								list.r#move(index, 1);
+								list.save(path);
+							}
+							if index != 0 { index -= 1; }
+						}
+						KeyCode::Down | KeyCode::Char('j') => {
+							if event.modifiers == KeyModifiers::CONTROL && index < list.len() - 1{
+								list.r#move(index, -1);
+								list.save(path);
+							}
+							if index != list.len() - 1 { index += 1; }
+						}
+						KeyCode::Char(' ') => {
+							list.toggle(index);
+							list.save(path);
+						}
+						KeyCode::Char('a') => {
+							//Append
+							list.append(index, Item { title: String::from(""), completed: false });
+							index = if list.len() == 1 {0} else {index + 1};
+							print_list!(list, index);
+							{
+								//Enter insert mode
+								loop {
+									let event = read()?;
+									if let Event::Key(code) = event {
+										if code.kind != KeyEventKind::Press {
+											continue;
+										}
+										let item: &mut Item = &mut list.items[index];
+										if code.code == KeyCode::Esc || code.code == KeyCode::Enter {
+											break;
+										} else if let KeyCode::Char(char) = code.code {
+											item.title.push(char);
+										} else if KeyCode::Backspace == code.code {
+											item.title.pop();
+										}
+										print_list!(list, index);
+									}
+								}
+
+							}
+							list.save(path);
+						}
+						KeyCode::Delete | KeyCode::Char('x') => {
+							// Delete selected
+							list.delete(index);
+							if index >= list.len(){
+								index = if index > 0 {index - 1} else {0};
+							}
+							list.save(path);
+						}
+						KeyCode::Esc => {
+							break;
+						}
+						_ => {}
+					}
+				}
+			},
+			Event::Mouse(_event) => {
+
+			},
+			Event::Paste(_data) => {
+				
+			},
+			Event::Resize(_width, _height) => {
+				
+			},
+		}
+
+		print_list!(list, index);
+	}
+	execute!(io::stdout(), LeaveAlternateScreen)?;
+
+	Ok(())
+}
 
 fn main() -> Result<(), io::Error>{
-    let args = Cli::parse();
+	let args = Cli::parse();
 
-    println!("{}", args.command);
-    return Ok(());
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    
-    fn clear(){
-        if cfg!(target_os = "windows") {
-            Command::new("cmd").arg("/C").arg("cls").status().expect("Can't execute 'cls' command");
-        } else {
-            Command::new("sh").arg("-C").arg("clear").status().expect("Can't execute 'clear' command");
-        }
-    }
-
-    let mut list : ToDoList = ToDoList::new();
-    let mut input = String::new();
-    let mut index : usize = 0; //Why not to save that also?
-
-    print_list!(list, index);
-    execute!(io::stdout(), EnterAlternateScreen)?;
-    let path = Path::new(".");
-    list.load(&path)?;
-    loop {
-        print!("\n> ");
-        let _ = io::stdout().flush();
-        input.clear();
-        // Need introduce modes
-        match read()? {
-            Event::FocusGained => {
-                
-            },
-            Event::FocusLost => {
-                
-            },
-            Event::Key(event) => {
-                if event.kind == KeyEventKind::Press  {
-                    match event.code {
-                        KeyCode::Up | KeyCode::Char('k') => {
-                            //If with CTRL, move element
-                            if event.modifiers == KeyModifiers::CONTROL {
-                                //assert, and move
-                            }
-                            if index != 0 { index -= 1; }
-                        }
-                        KeyCode::Down | KeyCode::Char('j') => {
-                            if index != list.items.len() - 1 { index += 1; }
-                        }
-                        KeyCode::Char(' ') => {
-                            list.toggle(index);
-                            list.save(path);
-                        }
-                        KeyCode::Char('a') => {
-                            //Append
-                            list.append(index, Item { title: String::from(""), completed: false });
-                            index = if list.len() == 1 {0} else {index + 1};
-                            print_list!(list, index);
-                            {
-                                //Enter insert mode
-                                loop {
-                                    let event = read()?;
-                                    if let Event::Key(code) = event {
-                                        if code.kind != KeyEventKind::Press {
-                                            continue;
-                                        }
-                                        let item: &mut Item = &mut list.items[index];
-                                        if code.code == KeyCode::Esc || code.code == KeyCode::Enter {
-                                            break;
-                                        } else if let KeyCode::Char(char) = code.code {
-                                            item.title.push(char);
-                                        } else if KeyCode::Backspace == code.code {
-                                            item.title.pop();
-                                        }
-                                        print_list!(list, index);
-                                    }
-                                }
-
-                            }
-                            list.save(path);
-                        }
-                        KeyCode::Delete | KeyCode::Char('x') => {
-                            // Delete selected
-                            list.delete(index);
-                            if index >= list.len(){
-                                index = if index > 0 {index - 1} else {0};
-                            }
-                            list.save(path);
-                        }
-                        KeyCode::Esc => {
-                            break;
-                        }
-                        _ => {}
-                    }
-                }
-            },
-            Event::Mouse(event) => {
-
-            },
-            Event::Paste(data) => {
-                
-            },
-            Event::Resize(width, height) => {
-                
-            },
-        }
-
-        print_list!(list, index);
-    }
-    execute!(io::stdout(), LeaveAlternateScreen)?;
-    Ok(())
+	if args.command == Some(Commands::Visual) {
+		return handle_visual(args);
+	} else {
+		return handle_cli(args);
+	}
 }
